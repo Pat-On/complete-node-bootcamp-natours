70c5c7b806225e34fc15fb3ea84e8997df267ea8
PatOn <mahomed2278@gmail.com>
2021-04-12 21:31:47 +0100

    refactoring routes

--- app.js
+++ app.js
@@ -3,41 +3,23 @@ const express = require('express');
 
 const app = express();
 
-//middleware
-//data from the body is added to the req body -> more about it later
 app.use(express.json()); // <-it really work nice!
 
-//app method "link"
-// app.get('/', (req, res) => {
-//   // res.status(200).send('Hello from the server side');
-
-//   res
-//     .status(200)
-//     .json({ message: 'Hello from the server side', app: 'Natours' });
-// });
-
-// app.post('/', (req, res) => {
-//   res.send('You can post to this endpoint');
-// });
-
-//reading the data from file
-// dev - datadata\datadata\data\tours - simple.json;
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
-app.get('/api/v1/tours', (req, res) => {
+const getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
-    //envelope: (enveloping)
+
     results: tours.length, // it is not exactly from specification but it is useful for front app to do it
     data: {
       tours: tours, //ES6 just tours - k and v the same
     },
   });
-});
-
-app.post('/api/v1/tours', (req, res) => {
+};
+const createTour = (req, res) => {
   // console.log(req);
   // console.log(req.body);
   const newId = tours[tours.length - 1].id + 1;
@@ -56,15 +38,11 @@ app.post('/api/v1/tours', (req, res) => {
       });
     }
   );
-  // res.send('done');
-});
+};
 
-app.get('/api/v1/tours/:id', (req, res) => {
+const getTour = (req, res) => {
   console.log(req.params);
   const id = req.params.id * 1; // trick in JS :>
-
-  //pseudo solution - over simplistic - in real world we will have
-  //  to check if the users code contain the proper input and if not contain "malwers"
 
   if (id > tours.length) {
     return res.status(404).json({
@@ -73,8 +51,6 @@ app.get('/api/v1/tours/:id', (req, res) => {
     });
   }
 
-  //another way around is to check if id is define so
-  //if (!tour) { ... }
   const tour = tours.find((el) => el.id === id);
 
   res.status(200).json({
@@ -84,9 +60,9 @@ app.get('/api/v1/tours/:id', (req, res) => {
       tour, //ES6 just tours - k and v the same
     },
   });
-});
+};
 
-app.patch('/api/v1/tours/:id', (req, res) => {
+const updateTour = (req, res) => {
   if (+req.params.id > tours.length) {
     return res.status(404).json({
       status: 'fail',
@@ -101,22 +77,37 @@ app.patch('/api/v1/tours/:id', (req, res) => {
       tour: '<Updated Tour here...>',
     },
   });
-});
+};
 
-app.delete('/api/v1/tours/:id', (req, res) => {
+const deleteTour = (req, res) => {
   if (+req.params.id > tours.length) {
     return res.status(404).json({
       status: 'fail',
       message: 'Invalid ID',
     });
   }
-  //we are not going to implement it here because it is to much work
-  // we will work on it base on the db
+
   res.status(204).json({
     status: 'success',
     data: null, //null mean that the data is no longer existing
   });
-});
+};
+
+app.route('/api/v1/tours').get(getAllTours).post(createTour);
+
+app
+  .route('/api/v1/tours/:id')
+  .get(getTour)
+  .patch(updateTour)
+  .delete(deleteTour);
+
+// app.get('/api/v1/tours', getAllTours);
+// app.post('/api/v1/tours', createTour);
+
+// app.get('/api/v1/tours/:id', getTour);
+// app.patch('/api/v1/tours/:id', updateTour);
+// app.delete('/api/v1/tours/:id', deleteTour);
+
 //starting the server
 const port = 3000;
 app.listen(port, () => {
