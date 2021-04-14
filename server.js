a0b92b6279f636138a4ee83b4794e9f5f0314b93
PatOn <mahomed2278@gmail.com>
2021-04-14 13:22:26 +0100

    connected mongoDB

--- server.js
+++ server.js
@@ -1,12 +1,29 @@
+const mongoose = require('mongoose');
+
 const dotenv = require('dotenv');
 
 //it only need to happen once and then process has all variables accessible from every single file in proj
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
-// console.log(app.get('env'));
-// console.log(process.env);
 // 4) starting the server
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    //we need to specify to take care for deprecation
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    // here we have ccess to the conection object
+    // console.log(con.connections);
+    console.log('DB connection successful!');
+  });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
