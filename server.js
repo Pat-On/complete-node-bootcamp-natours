edd14257f3710768d37c0e234152e43828e58881
PatOn <mahomed2278@gmail.com>
2021-04-14 14:15:10 +0100

    creating the simple tour model - schema -> model

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
-// 4) starting the server
 const DB = process.env.DATABASE.replace(
   '<PASSWORD>',
   process.env.DATABASE_PASSWORD
@@ -18,12 +17,36 @@ mongoose
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
+    useUnifiedTopology: true,
   })
   .then(() => {
-    // here we have ccess to the conection object
+    // here we have access to the connection object
     // console.log(con.connections);
     console.log('DB connection successful!');
   });
+
+//---------------------- mongoose ---------------------------
+const tourSchema = new mongoose.Schema({
+  //native for js types of data
+  // name: String,
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'], // <-proper name of it is validator <nice>
+    unique: true, // base on this we can not have two the same name of the trip
+  },
+  rating: {
+    type: Number,
+    default: 4.5, // default value if not specify
+  },
+  // price: Number,
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+
+// convention to use capital in that case
+const Tour = mongoose.model('Tour', tourSchema);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
