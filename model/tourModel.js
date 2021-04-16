f8540caeca47a10c7aea0f5de2e776c14c912a85
PatOn <mahomed2278@gmail.com>
2021-04-16 11:59:00 +0100

    v92 modelling the tours

--- model/tourModel.js
+++ model/tourModel.js
@@ -8,16 +8,55 @@ const tourSchema = new mongoose.Schema({
     type: String,
     required: [true, 'A tour must have a name'], // <-proper name of it is validator <nice>
     unique: true, // base on this we can not have two the same name of the trip
+    trim: true,
   },
-  rating: {
+  duration: {
+    type: Number,
+    required: [true, 'A tour must have a duration'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, ' A tour must have a group size'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must have a difficulty'],
+  },
+  ratingsAverage: {
     type: Number,
     default: 4.5, // default value if not specify
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
   },
   // price: Number,
   price: {
     type: Number,
     required: [true, 'A tour must have a price'],
   },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    trim: true, // only work for strings ->  different schema types different config
+    required: [true, 'A tour must have a description'],
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String, //e will store here just the reference to the picture, that is why it is string
+    required: [true, 'A tour must have a cover image'],
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(), //time stamp with a milliseconds in mong it is automatically converted to data
+  },
+  startDates: [Date],
+  //"2021-03-21" -> and mongo would pass it automatically to date
+  //if it is can not parse it, t would then it would throw error
 });
 
 // convention to use capital in that case - model declaration
