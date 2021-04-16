3a588d782e012d3801b8feb4aabd835d1a37a484
PatOn <mahomed2278@gmail.com>
2021-04-16 12:34:07 +0100

    first ever script - NICE

---
+++ dev-data/data/import-dev-data.js
@@ -0,0 +1,63 @@
+//independent file from our express application
+const fs = require('fs');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const Tour = require('../../model/tourModel');
+
+//it only need to happen once and then process has all variables accessible from every single file in proj
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    //we need to specify to take care for deprecation
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    // here we have access to the connection object
+    // console.log(con.connections);
+    console.log('DB connection successful!');
+  });
+
+// READ JSON FILE
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8')
+);
+
+// IMPORT DATA INTO DB
+
+const importData = async (req, res) => {
+  try {
+    await Tour.create(tours);
+    console.log('Data succesfully loaded');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+// DELETE ALL DATA FROM DB
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany();
+    console.log('Data delted');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+if (process.argv[2] === '--import') {
+  importData();
+} else if (process.argv[2] === '--delete') {
+  deleteData();
+}
+
+console.log(process.argv);
