d94d3d6f7cf8bd01961cbebdd3bdc75b06546546
PatOn <mahomed2278@gmail.com>
2021-04-19 06:50:37 +0100

    handling errors in production and development

--- controlers/errorController.js
+++ controlers/errorController.js
@@ -1,3 +1,33 @@
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+//STANDARD WAY OF DEALING IT
+const sendErrorProduction = (err, res) => {
+  //Operational, trusted error: send message to client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+    //Programming or other unknown error: don't leak error details
+  } else {
+    //1) Log error to console -> backend
+    console.error(err);
+
+    //2) send generic message
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went wrong',
+    });
+  }
+};
+
 module.exports = (err, req, res, next) => {
   //printing error stack trace
   //   console.log(err.stack);
@@ -6,9 +36,13 @@ module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   //the same like above
   err.status = err.status || 'error';
+
+  //distinguish between development and production
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrorProduction(err, res);
+  }
+
   //we are going to read the error from the error object
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
 };
