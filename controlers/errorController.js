c34f85346e1a48afce58716660e4e4bb69bba8bd
PatOn <mahomed2278@gmail.com>
2021-04-19 09:22:40 +0100

    duplicate database fields error handling code 11.000

--- controlers/errorController.js
+++ controlers/errorController.js
@@ -5,6 +5,18 @@ const handleCastErrorDB = (err) => {
   return new AppError(message, 400);
 };
 
+const handleDuplicateFieldsDB = (err) => {
+  const values = Object.values(err.keyValue).join(', '); // join if there is more values
+  const message = `Duplicate field value: ${values} Please use another value(s)!`;
+
+  return new AppError(message, 400);
+
+  //OLD CODE WHAT WE ARE NOT GETTING ANYMORE OUTDATED
+  // const value = err.errmsg.match(/(["'])(\\?.)*?\1/);
+  // console.log(value);
+  // const message = `Duplicate field value: {} Please use another value!`;
+};
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -25,7 +37,7 @@ const sendErrorProduction = (err, res) => {
     //Programming or other unknown error: don't leak error details
   } else {
     //1) Log error to console -> backend
-    console.error(err);
+    // console.error(err);
 
     //2) send generic message
     res.status(500).json({
@@ -38,7 +50,6 @@ const sendErrorProduction = (err, res) => {
 module.exports = (err, req, res, next) => {
   //printing error stack trace
   //   console.log(err.stack);
-
   //we are going to give default status code because we may have error
   err.statusCode = err.statusCode || 500;
   //the same like above
@@ -49,9 +60,12 @@ module.exports = (err, req, res, next) => {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === 'production') {
     let error = { ...err };
+    console.log(error.code);
     if (err.name === 'CastError') {
       error = handleCastErrorDB(error);
     }
+    // IMPORTANT
+    if (error.code === 11000) error = handleDuplicateFieldsDB(error);
 
     sendErrorProduction(error, res);
   }
