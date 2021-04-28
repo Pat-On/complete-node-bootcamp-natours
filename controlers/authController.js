dd9b5c52fe085ba973befe79983b392c091fa5f8
PatOn <mahomed2278@gmail.com>
2021-04-28 15:05:23 +0100

    silly mistake! - Coffee is neeeded !

--- controlers/authController.js
+++ controlers/authController.js
@@ -4,6 +4,7 @@ const jwt = require('jsonwebtoken');
 const User = require('../model/userModel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
+const sendEmail = require('../utils/email');
 
 console.log(process.env.JWT_EXPIRES_IN);
 const signToken = (id) =>
@@ -156,6 +157,47 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
   // await user.save(); // in this way it would give to use error because not all fields are provided
   await user.save({ validateBeforeSave: false });
   // 3 send it to user's email
+
+  //reset url
+  // const resetURL = `${req.protocol}://${req.get(
+  //   'host'
+  // )}/api/v1/users/resetPassword/${resetToken}`;
+
+  const resetURL = `${req.protocol}://${req.get(
+    'host'
+  )}/api/v1/users/resetPassword/${resetToken}`;
+
+  const message = `Forgot your password? Submit a PATCH request with your new password and passwordConfirm to: ${resetURL}. \n If you didn't forget your password, please ignore this email!`;
+
+  //he decided to use it because we need to do more than just catch the error
+  //like extension to the global middleware error handler!
+  try {
+    //sending email
+    await sendEmail({
+      email: user.email,
+      subject: 'Your password reset token (valid for 10min)',
+      message: message,
+    });
+
+    res.status(200).json({
+      status: 'success',
+      message: 'Token sent to email!',
+      // never put here reset token because everybody can take it. So we use email to make it more secure!
+    });
+  } catch (err) {
+    //we want here reset the token and expired
+    user.passwordResetToken = undefined;
+    user.passwordResetExpires = undefined;
+
+    await user.save({ validateBeforeSave: false });
+
+    return next(
+      new AppError(
+        'There was an error sending the email. Try again later!',
+        500
+      )
+    ); // error what happen on server
+  }
 });
 
 exports.resetPassword = (req, res, next) => {};
