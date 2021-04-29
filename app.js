cc217e013e43b6c795bab435141728de718708a2
PatOn <mahomed2278@gmail.com>
2021-04-29 11:06:33 +0100

    data sanitization

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controlers/errorController');
@@ -11,26 +14,38 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 console.log(process.env.NODE_ENV);
 //1) MIDDLEWARE GLOBAL
+//SET SECURITY HTTP HEADERS
+app.use(helmet());
+
+//DEVELOPMENT LOGGING
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
-
+// LIMIT REQUESTS FROM SAME API
 const limiter = rateLimit({
-  max: 100, // it should be set in elation to how API is used.
+  max: 100, // it should be set in relation to how API is used.
   windowMs: 60 * 60 * 1000,
   message: 'Too many requests from this IP, please try again in an hour',
 });
 
 app.use('/api', limiter);
 
-app.use(express.json()); // <-it really work nice!
+//BODY PARSER, READING DATA FRO< BODY into req.body
+app.use(express.json({ limit: '10kb' })); // <-it really work nice! if we have body larget thant 10 kb It would not be ccepted
+
+// DATA SANITIZATION AGAINST NoSQL query injection
+app.use(mongoSanitize());
+
+//DATA SANITIZATION against XSS
+app.use(xss());
 
 //serving the static files
 app.use(express.static(`${__dirname}/public`));
 
+// Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
-  console.log(req.headers);
+  // console.log(req.headers);
   next();
 });
 
