ac5d2e6f68af5dda2d0483a562c642057f2ac4c2
PatOn <mahomed2278@gmail.com>
2021-05-01 08:55:19 +0100

    modification in dev-data function

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -3,6 +3,8 @@ const fs = require('fs');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const Tour = require('../../model/tourModel');
+const User = require('../../model/userModel');
+const Review = require('../../model/reviewModel');
 
 //it only need to happen once and then process has all variables accessible from every single file in proj
 dotenv.config({ path: './config.env' });
@@ -28,13 +30,19 @@ mongoose
 
 // READ JSON FILE
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'));
-
+const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, 'utf-8'));
+const reviews = JSON.parse(
+  fs.readFileSync(`${__dirname}/reviews.json`, 'utf-8')
+);
 // IMPORT DATA INTO DB
 
 const importData = async (req, res) => {
   try {
     await Tour.create(tours);
-    console.log('Data succesfully loaded');
+    await Review.create(reviews);
+    await User.create(users, { validateBeforeSave: false });
+
+    console.log('Data successfully loaded');
   } catch (err) {
     console.log(err);
   }
@@ -45,7 +53,9 @@ const importData = async (req, res) => {
 const deleteData = async () => {
   try {
     await Tour.deleteMany();
-    console.log('Data delted');
+    await User.deleteMany();
+    await Review.deleteMany();
+    console.log('Data deleted');
   } catch (err) {
     console.log(err);
   }
