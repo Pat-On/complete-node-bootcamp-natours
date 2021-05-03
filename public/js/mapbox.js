e3087f8729fad3348f498d181bdcca37dd6aed6e
PatOn <mahomed2278@gmail.com>
2021-05-03 09:30:28 +0100

    mapbox first step

---
+++ public/js/mapbox.js
@@ -0,0 +1,5 @@
+/* eslint-disable */
+
+// we are reading the data what we before put into the html dom
+const locations = JSON.parse(document.getElementById('map').dataset.locations);
+console.log(locations);
