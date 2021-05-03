32a84c4fa1895fdfa5f1dc8d432d6ee670fdb522
PatOn <mahomed2278@gmail.com>
2021-05-03 09:50:06 +0100

    fixed bug - mapbox - viewController/getTour - res.set

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -24,8 +24,14 @@ exports.getTour = catchAsync(async (req, res) => {
 
   // 3) Render that template using tour data from 1)
 
-  res.status(200).render('tour', {
-    title: `${tour.name} Tour`,
-    tour,
-  });
+  res
+    .status(200)
+    .set(
+      'Content-Security-Policy',
+      'connect-src https://*.tiles.mapbox.com https://api.mapbox.com https://events.mapbox.com'
+    )
+    .render('tour', {
+      title: `${tour.name} Tour`,
+      tour,
+    });
 });
--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -3,3 +3,10 @@
 // we are reading the data what we before put into the html dom
 const locations = JSON.parse(document.getElementById('map').dataset.locations);
 console.log(locations);
+
+mapboxgl.accessToken =
+  'pk.eyJ1IjoicGF0b24iLCJhIjoiY2tvOGNwMnZqMmwzcDJ3bXY3ZGVtZmJvdiJ9.57EU7u1-iAhK6QXzdvy-xg';
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/streets-v11',
+});
