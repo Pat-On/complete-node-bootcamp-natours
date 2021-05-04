e69bd95037843356a817c8477d00a1502bcbb820
PatOn <mahomed2278@gmail.com>
2021-05-04 15:45:35 +0100

    complexe email handler

--- utils/email.js
+++ utils/email.js
@@ -1,36 +1,99 @@
 const nodemailer = require('nodemailer');
 
-const sendEmail = async (options) => {
-  // 1) create a transporter - service which is going to send the email
-  const transporter = nodemailer.createTransport({
-    // service: 'Gmail',
-    // auth: {
-    //   user: process.env.EMAIL_USERNAME,
-    //   pass: process.env.EMAIL.PASSWORD,
-    // },
-    // Activate in gmail "less secure app" option -> to check it out here
-    // we are not using gmail, because it is not best idea for production app
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    // secure: false,
-    auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD,
-    },
-  });
+const pug = require('pug');
 
-  // 2) define the email options
-  const mailOptions = {
-    from: 'Patryk Nowak <hello@patryk.io>',
-    to: options.email,
-    subject: options.subject,
-    text: options.message,
-    // we can compile it to html -> we will do it later
-  };
+const htmlToText = require('html-to-text');
+// new Email(user, url).sendWelcome();   .sendPasswordReset
 
-  // 3) actually send the email with nodemailer
-  await transporter.sendMail(mailOptions);
+module.exports = class Email {
+  constructor(user, url) {
+    this.to = user;
+    this.firstName = user.name.split(' ')[0];
+    this.url = url;
+    this.from = `Patryk <${process.env.EMAIL_FROM}>`;
+  }
+
+  // it make very easy to make different transports for different needs
+  newTransport() {
+    if (process.env.NODE_ENV === 'production') {
+      //Sendgrid
+
+      return 1;
+    }
+    return nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      // secure: false,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
+  }
+
+  //Send the actual email
+  async send(template, subject) {
+    //1 ) Render HTML based on a pug template
+    // res.render('')
+    const html = pug.renderFile(
+      `${__dirname}/../views/emails/${template}.pug`,
+      {
+        firstName: this.first,
+        url: this.url,
+        subject,
+      }
+    );
+    // 2) define email options
+
+    const mailOptions = {
+      from: this.from,
+      to: this.to,
+      subject: subject,
+      test: htmlToText.fromString(html),
+      html,
+    };
+
+    // 3) create a transport and send email
+    await this.newTransport().sendMail(mailOptions);
+  }
+
+  //only for send welcome
+  async sendWelcome() {
+    await this.send('Welcome', 'Welcome to the Natours Family!');
+  }
 };
 
+// const sendEmail = async (options) => {
+//   // 1) create a transporter - service which is going to send the email
+//   // const transporter = nodemailer.createTransport({
+//   //   // service: 'Gmail',
+//   //   // auth: {
+//   //   //   user: process.env.EMAIL_USERNAME,
+//   //   //   pass: process.env.EMAIL.PASSWORD,
+//   //   // },
+//   //   // Activate in gmail "less secure app" option -> to check it out here
+//   //   // we are not using gmail, because it is not best idea for production app
+//   //   host: process.env.EMAIL_HOST,
+//   //   port: process.env.EMAIL_PORT,
+//   //   // secure: false,
+//   //   auth: {
+//   //     user: process.env.EMAIL_USERNAME,
+//   //     pass: process.env.EMAIL_PASSWORD,
+//   //   },
+//   // });
+
+//   // 2) define the email options
+//   const mailOptions = {
+//     from: 'Patryk <hello@patryk.io>',
+//     to: options.email,
+//     subject: options.subject,
+//     text: options.message,
+//     // we can compile it to html -> we will do it later
+//   };
+
+//   // 3) actually send the email with nodemailer
+//   await transporter.sendMail(mailOptions);
+// };
+
 //default export
-module.exports = sendEmail;
+// module.exports = sendEmail;
