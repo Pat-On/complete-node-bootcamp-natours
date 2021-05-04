232ab7fce0916140382cf3df26c4fea0085f5d92
PatOn <mahomed2278@gmail.com>
2021-05-04 18:25:45 +0100

    reset password email template pug

--- controllers/authController.js
+++ controllers/authController.js
@@ -251,23 +251,23 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
   const resetToken = user.createPasswordResetToken();
   // await user.save(); // in this way it would give to use error because not all fields are provided
   await user.save({ validateBeforeSave: false });
-  // 3 send it to user's email
 
-  const resetURL = `${req.protocol}://${req.get(
-    'host'
-  )}/api/v1/users/resetPassword/${resetToken}`;
-
-  const message = `Forgot your password? Submit a PATCH request with your new password and passwordConfirm to: ${resetURL}. \n If you didn't forget your password, please ignore this email!`;
+  // const message = `Forgot your password? Submit a PATCH request with your new password and passwordConfirm to: ${resetURL}. \n If you didn't forget your password, please ignore this email!`;
 
   //he decided to use it because we need to do more than just catch the error
   //like extension to the global middleware error handler!
+  // 3 send it to user's email
   try {
+    const resetURL = `${req.protocol}://${req.get(
+      'host'
+    )}/api/v1/users/resetPassword/${resetToken}`;
     //sending email
     // await sendEmail({
     //   email: user.email,
     //   subject: 'Your password reset token (valid for 10min)',
     //   message: message,
     // });
+    await new Email(user, resetURL).sendPasswordReset();
 
     res.status(200).json({
       status: 'success',
